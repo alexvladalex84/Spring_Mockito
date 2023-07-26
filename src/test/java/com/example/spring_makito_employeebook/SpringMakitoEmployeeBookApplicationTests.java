2f7294072120604fc155e7c7f7850300b713af8a
Alexandrov <alex.44747.alex@gmail.com>
2023-07-26 15:35:44 +0300

    Домашка с Моками

---
+++ src/test/java/com/example/spring_makito_employeebook/SpringMakitoEmployeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_makito_employeebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMakitoEmployeeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
